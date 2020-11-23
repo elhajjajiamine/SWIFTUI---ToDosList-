ac7b4c067eb1448e0566cb205388db1c6d8c5a0b
elhajjajiamine <aminselhajjaji@gmail.com>
2020-11-23 10:47:00 +0100

    Initial Commit

---
+++ ToDosList/ToDosListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDosListApp.swift
+//  ToDosList
+//
+//  Created by elhajjaji on 23/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ToDosListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
